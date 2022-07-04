dded920c440c4793bd2a3ecaaa3a2d3e15d3648b
dsweb-cjop2 <christopher29739570@gmail.com>
2022-07-04 16:42:18 -0400

    Prueba

--- src/app/Portafolio/portafolio.module.ts
+++ src/app/Portafolio/portafolio.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
 import { HomeComponent } from './pages/home/home.component';
+import { CertificatesComponent } from './pages/certificates/certificates.component';
+import { PortfolioComponent } from './pages/portfolio/portfolio.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
+
 
 
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, CertificatesComponent, PortfolioComponent, ContactoComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
-  exports:[HomeComponent]
+  exports:[HomeComponent, CertificatesComponent, PortfolioComponent, ContactoComponent]
 })
 export class PortafolioModule { }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [
 
+
+import { HomeComponent } from './Portafolio/pages/home/home.component';
+import { CertificatesComponent } from './Portafolio/pages/certificates/certificates.component';
+import { PortfolioComponent } from './Portafolio/pages/portfolio/portfolio.component';
+
+import { ContactoComponent } from './Portafolio/pages/contacto/contacto.component';
+
+const routes: Routes = [
+  {
+    path:'',
+    component: HomeComponent,
+    pathMatch: 'full'
+  },
+  {
+    path:'Certificates',
+    component: CertificatesComponent
+  },
+  {
+    path:'Portfolio',
+    component: PortfolioComponent
+  },
+  {
+    path:'Contacto',
+    component: ContactoComponent
+  }
 ];
 
 @NgModule({
